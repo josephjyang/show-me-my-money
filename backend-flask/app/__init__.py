7620d9fb6597dd81d8197d53bdfe44079612000f
Joe Yang <josephjyang@gmail.com>
2022-01-19 11:20:29 -0800

    backend set up WIP

---
+++ backend-flask/app/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template, request, session, redirect
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import LoginManager
+
+from .models import db, User
+from .api.user_routes import user_routes
+from .api.auth_routes import auth_routes
+
+from .seeds import seed_comands
+from .config import Config
+
+app = Flask(__name__)
+
+login = LoginManager(app)
+login.login_view = 'auth.unauthorized'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+app.cli.add_command(seed_commands)
---
+++ backend-flask/app/forms/login_form.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email, ValidationError
+from app.models import User
+
+
+def user_exists(form, field):
+    credential = field.data
+    user = User.query.filter(User.email == credential).first()
+    if not user:
+        user = User.query.filter(User.username == credential).first()
+        if not user:
+            raise ValidationError('Email/username provided not found')
+
+
+def password_matches(form, field):
+    password = field.data
+    credential = form.data['credential']
+    user = User.query.filter(User.email == credential).first()
+    if not user:
+        user = User.query.filter(User.username == credential).first()
+        if not user:
+            raise ValidationError('Email/username provided not found')
+    if not user.check_password(password):
+        raise ValidationError('Password was incorrect.')
+
+
+class LoginForm(FlaskForm):
+    credential = StringField('credential', validators=[
+                             DataRequired(), user_exists])
+    password = StringField('password', validators=[
+                           DataRequired(), password_matches])
