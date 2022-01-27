86e83efbdedfad12ccd4bf4b457cc08c272717cc
Joe Yang <josephjyang@gmail.com>
2022-01-27 14:26:45 -0800

    added function for elapsed time

--- frontend-react/src/components/Newsfeed/index.js
+++ frontend-react/src/components/Newsfeed/index.js
@@ -26,6 +26,49 @@ function Newsfeed({ person }) {
         else return transaction.paid
     })
 
+    const passedTime = (transaction) => {
+        let startTime = new Date(transaction.updated_at);
+        startTime.setHours(startTime.getHours() + 8)
+        let endTime = new Date();
+        
+        // time difference in ms
+        let timeDiff = endTime.getTime() - startTime.getTime();
+        // strip the ms
+        timeDiff /= 1000;
+
+        // get seconds
+        let seconds = Math.round(timeDiff % 60);
+
+        // remove seconds from the date
+        timeDiff = Math.floor(timeDiff / 60);
+
+        // get minutes
+        let minutes = Math.round(timeDiff % 60);
+
+        // remove minutes from the date
+        timeDiff = Math.floor(timeDiff / 60);
+
+        // get hours
+        let hours = Math.round(timeDiff % 24);
+
+        // remove hours from the date
+        timeDiff = Math.floor(timeDiff / 24);
+
+        let days = timeDiff;
+        let totalHours = hours + (days * 24); // add days to hours
+        if (minutes === 0) {
+            return seconds + "s"
+        } else if (totalHours === 0) {
+            return minutes + "m";
+        } else if (days === 0) {
+            return totalHours + "h";
+        } else if (days < 7) {
+            return days + "d"
+        } else if (days < 365) {
+            return Math.floor(days / 7) + "w";
+        } else return Math.floor(days / 365) + "y";
+    }
+
     const dispatch = useDispatch();
     useEffect(() => {
         if (user) {
@@ -88,6 +131,9 @@ function Newsfeed({ person }) {
                                         {transaction.payer_id === user.id && <div className="neg-amount"> -${transaction.amount % 1 !== 0 ? Intl.NumberFormat('en-US').format(transaction.amount) : Intl.NumberFormat('en-US').format(transaction.amount) + ".00"}</div>}
                                         {transaction.payee_id === user.id && <div className="pos-amount"> +${transaction.amount % 1 !== 0 ? Intl.NumberFormat('en-US').format(transaction.amount) : Intl.NumberFormat('en-US').format(transaction.amount) + ".00"}</div>}
                                     </div>
+                                    <p>
+                                        {passedTime(transaction)}
+                                    </p>
                                     <div className="transaction-details">
                                         {transaction.details}
                                     </div>
