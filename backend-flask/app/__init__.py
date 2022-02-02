3313bd9afdaa3a370e8c83deafd4f0a23b92a845
Joe Yang <josephjyang@gmail.com>
2022-02-02 10:51:03 -0800

    splash path

--- backend-flask/app/__init__.py
+++ backend-flask/app/__init__.py
@@ -66,8 +66,6 @@ def inject_csrf_token(response):
 def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
-    if path == 'smmm-logo.png':
-        return app.send_static_file('smmm-logo.png')
-    if path == 'smmm-sign.png':
-        return app.send_static_file('smmm-sign.png')
+    if path == 'splash.png':
+        return app.send_static_file('splash.png')
     return app.send_static_file('index.html')
