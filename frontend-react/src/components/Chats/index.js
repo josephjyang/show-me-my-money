e8ebfa95a4c8fea402ca9936b86918db2a61198a
Joe Yang <josephjyang@gmail.com>
2022-02-10 11:14:58 -0800

    styling, add search bar

--- frontend-react/src/components/Chats/index.js
+++ frontend-react/src/components/Chats/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getChats } from '../../store/chats';
 import Chat from '../Chat';
+import FriendsSearchBar from '../FriendsSearch';
+import './Chats.css';
 
 const Chats = () => {
     const user = useSelector(state => state.session.user);
@@ -16,8 +18,10 @@ const Chats = () => {
     return (user && (
         <div id="chats-container">
             <div id="chats-list">
+                <h2>Messages</h2>
+                <FriendsSearchBar />
                 {chats.map((chat, ind) => (
-                    <div key={ind} onClick={() => setChatroom(chat)}>
+                    <div className="chat-container" key={ind} onClick={() => setChatroom(chat)}>
                         <div className="message-name">
                             {`${chat.user.id === user.id ? `${chat.friend.first_name} ${chat.friend.last_name}`: `${chat.user.first_name} ${chat.user.last_name}`}`}
                         </div>
---
+++ frontend-react/src/components/FriendsSearch/index.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { useMode } from '../../context/AppContext';
+import { getFriends } from '../../store/friends';
+
+const filterFriends = (users, query) => {
+    if (!query) {
+        return users;
+    }
+
+    return users.filter(user => {
+        const username = user.username.toLowerCase();
+        const firstName = user.first_name.toLowerCase();
+        const lastName = user.last_name.toLowerCase();
+        const term = query.toLowerCase()
+        return username.includes(term) || firstName.includes(term) || lastName.includes(term);
+    });
+};
+
+const FriendsSearchBar = () => {
+    const user = useSelector(state => state.session.user);
+    const stateFriends = useSelector(state => state.friends);
+    const friends = Object.values(stateFriends);
+    const { dark } = useMode();
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchQuery, setSearchQuery] = useState(query || '');
+    const filteredFriends = filterFriends(friends, searchQuery);
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (user) {
+            dispatch(getFriends(user));
+        }
+    }, [dispatch, user])
+
+
+    return (
+        <div id="users-search-ctr">
+            <form
+                action="/"
+                method="get"
+                autoComplete="on"
+                id="users-search"
+            >
+                <label htmlFor="header-search">
+                    <span className="visually-hidden">
+                        Search users
+                    </span>
+                </label>
+                <i className={`fas fa-search ${dark}`}></i>
+                <input
+                    value={searchQuery}
+                    onInput={(e) => setSearchQuery(e.target.value)}
+                    type="text"
+                    id="user-header-search"
+                    name="s"
+                    className={dark}
+                    placeholder="Search for friends"
+                />
+            </form>
+            <div id="users-search-results" className={dark}>
+                {searchQuery && filteredFriends.map(user => (
+                    <NavLink className="search-links" to={`/users/${user.id}`} key={user.id} >
+                        <div className="user-card">
+                            {user?.profile_pic ? <img className="creator-picture" src={user.profile_pic} alt="creator" /> : <div className="replacement-photo">{user?.first_name[0]}-{user?.last_name[0]}</div>}
+                            <div className="user-card-info">
+                                <div className="user-card-name">{user.first_name} {user.last_name}</div>
+                                <div className="user-card-name">@{user.username}</div>
+                            </div>
+                        </div>
+                    </NavLink>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default FriendsSearchBar;
